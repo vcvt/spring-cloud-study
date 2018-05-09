76feeebdfa21be119252001047f3f7db4fd0e3bc
smmit <smmith@foxmail.com>
2018-05-10 00:07:11 +0800

    简单天气预报系统完成

--- src/main/java/com/waylau/spring/cloud/weather/vo/WeatherResponse.java
+++ src/main/java/com/waylau/spring/cloud/weather/vo/WeatherResponse.java
@@ -6,23 +6,23 @@ import java.io.Serializable;
  * @author smmit
  * @date 2018-05-08
  */
-public class WeatherResponse implements Serializable{
-        private static final long serialVersionUID = 3497969326478158206L;
-        private Weather data;
-        private Integer status;
-        private String desc;
+public class WeatherResponse implements Serializable {
+    private static final long serialVersionUID = 3497969326478158206L;
+    private Weather data;
+    private Integer status;
+    private String desc;
 
-        public Weather getData() {
-            return data;
-        }
+    public Weather getData() {
+        return data;
+    }
 
-        public void setData(Weather data) {
-            this.data = data;
-        }
+    public void setData(Weather data) {
+        this.data = data;
+    }
 
-        public Integer getStatus() {
-            return status;
-        }
+    public Integer getStatus() {
+        return status;
+    }
 
     public void setStatus(Integer status) {
         this.status = status;
